3c7b4dfe9d9ddda87bfd039c89a0e8b1dd59a5eb
Eduardo Winter <eduardo.wbess@outlook.commit>
2021-01-26 11:17:46 -0300

    Error handling

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -22,9 +22,15 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    delete user.password;
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
 
-    return response.json(user);
+    return response.json(userWithoutPassword);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -43,11 +49,17 @@ usersRouter.patch(
         avatarFilename: request.file.filename,
       });
 
-      delete user.password;
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
 
-      return response.json(user);
+      return response.json(userWithoutPassword);
     } catch (err) {
-      return response.status(400).json({ error: err.message });
+      return response.status(err.statusCode).json({ error: err.message });
     }
   },
 );
